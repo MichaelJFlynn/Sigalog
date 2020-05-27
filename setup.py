00c21f8589da5f22fa4d459cd5d9b5594114e243
mikedev <mflynn210@gmail.com>
2020-05-27 02:09:30 +0000

    update version number

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name="Sigalog",
-      version="0.1",
+      version="0.2",
       description="Easy URL signing",
       url="https://github.com/MichaelJFlynn/Sigalog",
       author="Michael J. Flynn",
